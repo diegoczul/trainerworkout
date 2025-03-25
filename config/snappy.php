224a4a2ed07399139a4a276a3971ecbdf8bb624d
Krish siddhapura <krish.siddhapura@grewon.com>
2025-03-25 10:13:21 +0530

    change in .env.example file and snappy configs

--- config/snappy.php
+++ config/snappy.php
@@ -1,37 +1,18 @@
 <?php
 
-if (env('APP_ENV') == 'local'){
-    return [
-        'pdf' => array(
-            'enabled' => true,
-            'binary' => "/usr/local/bin/wkhtmltopdf",
-            'timeout' => false,
-            'options' => array(),
-            'env' => array(),
-        ),
-        'image' => array(
-            'enabled' => true,
-            'binary' => "/usr/local/bin/wkhtmltoimage",
-            'timeout' => false,
-            'options' => array(),
-            'env' => array(),
-        ),
-    ];
-}else{
-    return [
-        'pdf' => array(
-            'enabled' => true,
-            'binary' => "/usr/bin/xvfb-run -a /usr/bin/wkhtmltopdf",
-            'timeout' => 300,
-            'options' => array(),
-            'env' => array(),
-        ),
-        'image' => array(
-            'enabled' => true,
-            'binary' => "/usr/bin/xvfb-run -a /usr/bin/wkhtmltoimage",
-            'timeout' => 300,
-            'options' => array(),
-            'env' => array(),
-        ),
-    ];
-}
+return [
+    'pdf' => array(
+        'enabled' => true,
+        'binary' => env('WKHTML_PDF_BINARY', '/usr/bin/xvfb-run -a /usr/bin/wkhtmltopdf'),
+        'timeout' => false,
+        'options' => array(),
+        'env' => array(),
+    ),
+    'image' => array(
+        'enabled' => true,
+        'binary' => env('WKHTML_IMG_BINARY', '/usr/bin/xvfb-run -a /usr/bin/wkhtmltoimage'),
+        'timeout' => false,
+        'options' => array(),
+        'env' => array(),
+    ),
+];
