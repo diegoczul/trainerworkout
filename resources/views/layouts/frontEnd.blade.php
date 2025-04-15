0bd22ed9f2ac9079485f4f57d6308623e605148c
Krish siddhapura <krish.siddhapura@grewon.com>
2025-04-15 16:13:21 +0530

    commented hotjar script

--- resources/views/layouts/frontEnd.blade.php
+++ resources/views/layouts/frontEnd.blade.php
@@ -62,19 +62,19 @@
     @endif
 
     <!-- Hotjar Tracking Code for www.trainerworkout.com -->
-    <script>
-        (function (h, o, t, j, a, r) {
-            h.hj = h.hj || function () {
-                (h.hj.q = h.hj.q || []).push(arguments)
-            };
-            h._hjSettings = {hjid: 9600, hjsv: 5};
-            a = o.getElementsByTagName('head')[0];
-            r = o.createElement('script');
-            r.async = 1;
-            r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-            a.appendChild(r);
-        })(window, document, '//static.hotjar.com/c/hotjar-', '.js?sv=');
-    </script>
+{{--    <script>--}}
+{{--        (function (h, o, t, j, a, r) {--}}
+{{--            h.hj = h.hj || function () {--}}
+{{--                (h.hj.q = h.hj.q || []).push(arguments)--}}
+{{--            };--}}
+{{--            h._hjSettings = {hjid: 9600, hjsv: 5};--}}
+{{--            a = o.getElementsByTagName('head')[0];--}}
+{{--            r = o.createElement('script');--}}
+{{--            r.async = 1;--}}
+{{--            r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;--}}
+{{--            a.appendChild(r);--}}
+{{--        })(window, document, '//static.hotjar.com/c/hotjar-', '.js?sv=');--}}
+{{--    </script>--}}
 </head>
 <body>
 <div id="systemMessages" class="systemMessages"></div>
