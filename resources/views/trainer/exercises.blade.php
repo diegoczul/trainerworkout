e147256dc8a0ab95275196b8b7de61ddededfea9
Aashutosh Telpure <aashutosh.t@grewon.com>
2025-05-01 18:09:36 +0530

    my exercises edit page design changes

--- resources/views/trainer/exercises.blade.php
+++ resources/views/trainer/exercises.blade.php
@@ -112,7 +112,7 @@
                             <div id="textarea_counter"></div>
                         </fieldset>
 
-                        <div class="equipment mb-6">
+                        <div class="equipment select-equipment-section mb-6">
                             <label for="muscleGroup" tabindex="3">{{ Lang::get("content.Musclegroups") }}*</label>
                             {{ FORM::select("bodygroupId", [""=>Lang::get("content.MuscleGroupChoose")] + $bodygroups->toArray(), request()->old("bodygroup"), ["id"=>"muscleGroup", "data-placeholder"=> Lang::get("content.selectequipment"), "class"=>"select2-select w-100"]) }}
                         </div>
@@ -168,7 +168,7 @@
 
                         </fieldset>
                         <div class="previous">
-                            <span onclick="addExerciseSwtichStep()"><div class="previousButton"><img src="{{asset('assets/img/svg/arrowPrevious.svg')}}"></div> previous step</span>
+                            <span onclick="addExerciseSwtichStep()" class="previous-section"><div class="previousButton"><img src="{{asset('assets/img/svg/arrowPrevious.svg')}}"></div> previous step</span>
                         </div>
                         <div class="submit">
                             <button type="submit" class="saveex" onClick="lightBoxLoadingTwSpinner();">{{ Lang::get("content.CreateExercise") }}</button>
