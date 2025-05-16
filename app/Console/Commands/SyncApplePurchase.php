98bf29865c1ec1dd3ecc818bff9ef9ec03a758c0
Krish siddhapura <krish.siddhapura@grewon.com>
2025-05-16 19:49:39 +0530

    Update SyncApplePurchase.php

--- app/Console/Commands/SyncApplePurchase.php
+++ app/Console/Commands/SyncApplePurchase.php
@@ -33,53 +33,75 @@ class SyncApplePurchase extends Command
      */
     public function handle()
     {
-        AppleNotification::where('is_verified',0)->get()->each(function ($notification) {
-            $original_transaction_id = $notification->original_transaction_id;
-            $transaction_id = $notification->transaction_id;
-            $expiry_date = $notification->expiry_date;
-            $MembershipsUsers = MembershipsUsers::where([
-                'apple_original_transaction_id' => $original_transaction_id,
-                'deleted_at' => null
-            ])->count();
-            if ($MembershipsUsers > 0){
-                if ($notification->notification_type == 'EXPIRED'){
-                    $users = MembershipsUsers::where([
-                        'apple_original_transaction_id' => $original_transaction_id,
-                        'deleted_at' => null
-                    ])->pluck('userId');
+        $AppleNotificationId = AppleNotification::selectRaw('MAX(id) as max_id')
+            ->where('is_verified',0)
+            ->groupBy('original_transaction_id')
+            ->pluck('max_id');
 
-                    // REMOVE ALL OLD MEMBERSHIPS
-                    MembershipsUsers::where([
-                        'apple_original_transaction_id' => $original_transaction_id,
-                        'deleted_at' => null
-                    ])->delete();
+        if (!empty($AppleNotificationId)){
+            $AppleNotification = AppleNotification::whereIn('id',$AppleNotificationId)->get()->toArray();
 
-                    // ADD TRIAL MEMBERSHIP
-                    foreach ($users as $user){
-                        $u = Users::find($user);
-                        $u->updateToMembership(Config::get('constants.freeTrialMembershipId'));
+            foreach($AppleNotification as $notification) {
+                $original_transaction_id = $notification['original_transaction_id'];
+                $transaction_id = $notification['transaction_id'];
+                $expiry_date = $notification['expiry_date'];
+                $MembershipsUsers = MembershipsUsers::where([
+                    'apple_original_transaction_id' => $original_transaction_id,
+                    'deleted_at' => null
+                ])->count();
+                if ($MembershipsUsers > 0){
+                    if ($notification['notification_type'] == 'EXPIRED'){
+                        $users = MembershipsUsers::where([
+                            'apple_original_transaction_id' => $original_transaction_id,
+                            'deleted_at' => null
+                        ])->pluck('userId');
+
+                        // REMOVE ALL OLD MEMBERSHIPS
+                        MembershipsUsers::where([
+                            'apple_original_transaction_id' => $original_transaction_id,
+                            'deleted_at' => null
+                        ])->delete();
+
+                        // ADD TRIAL MEMBERSHIP
+                        foreach ($users as $user){
+                            $u = Users::find($user);
+                            $u->updateToMembership(Config::get('constants.freeTrialMembershipId'));
+                        }
+                    }else{
+                        // UPDATE MEMBERSHIP EXPIRY
+                        MembershipsUsers::where([
+                            'apple_original_transaction_id' => $original_transaction_id,
+                            'deleted_at' => null
+                        ])->update(['expiry' => Carbon::parse($expiry_date)]);
                     }
+
+                    // UPDATE USER TRANSACTION STATUS
+                    UserApplePurchaseTransaction::where([
+                        'original_transaction_id' => $original_transaction_id,
+                        'transaction_id' => $transaction_id,
+                    ])->update(['is_verified' => 1]);
+
+                    // UPDATE NOTIFICATION VERIFICATION STATUS
+                    AppleNotification::where('id','<=',$notification['id'])
+                        ->where('original_transaction_id',$original_transaction_id)
+                        ->update(['is_verified' => 1]);
                 }else{
-                    // UPDATE MEMBERSHIP EXPIRY
-                    MembershipsUsers::where([
-                        'apple_original_transaction_id' => $original_transaction_id,
-                        'deleted_at' => null
-                    ])->update(['expiry' => Carbon::parse($expiry_date)]);
+                    // UPDATE USER TRANSACTION STATUS
+                    UserApplePurchaseTransaction::where([
+                        'original_transaction_id' => $original_transaction_id,
+                        'transaction_id' => $transaction_id,
+                    ])->update(['is_verified' => 1]);
+
+                    // UPDATE NOTIFICATION VERIFICATION STATUS
+                    AppleNotification::where('id','<=',$notification['id'])
+                        ->where('original_transaction_id',$original_transaction_id)
+                        ->update(['is_verified' => 2]);
+
+                    Log::driver('apple_purchase_log')->debug("NOT FOUND");
+                    Log::driver('apple_purchase_log')->debug("[$original_transaction_id]($transaction_id) --- ($expiry_date)");
                 }
+            };
+        }
 
-                // UPDATE USER TRANSACTION STATUS
-                UserApplePurchaseTransaction::where([
-                    'original_transaction_id' => $original_transaction_id,
-                    'transaction_id' => $transaction_id,
-                ])->update(['is_verified' => 1]);
-
-                // UPDATE NOTIFICATION VERIFICATION STATUS
-                $notification->is_verified = 1;
-                $notification->save();
-            }else{
-                Log::driver('apple_purchase_log')->debug("NOT FOUND");
-                Log::driver('apple_purchase_log')->debug("[$original_transaction_id]($transaction_id) --- ($expiry_date)");
-            }
-        });
     }
 }
