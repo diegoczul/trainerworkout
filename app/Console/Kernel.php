047e7084414ff6c2cc36bbcc6ecd7006ff632f3d
Krish siddhapura <krish.siddhapura@grewon.com>
2025-05-20 17:30:25 +0530

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('plans:sync-cancellations')->everyTenMinutes();
         $schedule->command('sync:stripe-transactions')->dailyAt('02:00'); // 2AM server time
         $schedule->command('app:release-frozen-earnings')->daily();
-        $schedule->command('sync:apple-transactions')->daily();
+        $schedule->command('sync:apple-purchase')->daily();
         $schedule->call(function () {
             Log::info('Cron is working');
         })->everyMinute();
